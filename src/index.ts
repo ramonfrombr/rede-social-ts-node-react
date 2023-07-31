3785e8c24b1d9b7feb7119d0629c42eb3ea58200
ramonfrombr@gmail.com <ramonfrombr>
2023-07-30 23:38:38 -0300

    02. configuracao express, middlewares, e seus tipos

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Olá, mundo!")
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.listen(8080, () => {
+    console.log("Servidor backend rodando!")
+})
